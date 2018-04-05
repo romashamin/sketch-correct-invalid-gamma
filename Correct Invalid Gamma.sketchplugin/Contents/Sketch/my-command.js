605d2bce2403743501b95cc39052aa933a8acb9b
Roman Shamin <al4emist.artway@gmail.com>
2018-04-05 10:58:22 +0300

    Add .sketchplugin file

---
+++ Correct Invalid Gamma.sketchplugin/Contents/Sketch/my-command.js
@@ -0,0 +1,123 @@
+var that = this;
+function __skpm_run (key, context) {
+  that.context = context;
+
+var exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports['default'] = function (context) {
+  var selectedLayers = context.selection;
+
+  if (selectedLayers.length === 0) {
+    context.document.showMessage('Select at least one bitmap');
+  } else {
+    var predicateBitmapLayers = NSPredicate.predicateWithFormat("self isMemberOfClass:%@", MSBitmapLayer['class']());
+    var bitmapLayers = selectedLayers.filteredArrayUsingPredicate(predicateBitmapLayers);
+
+    var bitmapLayersLength = bitmapLayers.length;
+
+    if (bitmapLayersLength === 0) {
+      context.document.showMessage('Select at least one bitmap');
+    } else {
+
+      bitmapLayers.forEach(function (layer) {
+        return layer.correctInvalidGamma();
+      });
+
+      if (bitmapLayersLength === 1) {
+        context.document.showMessage('The bitmap has been corrected');
+      } else {
+        context.document.showMessage(String(bitmapLayersLength) + ' bitmaps have been corrected');
+      }
+    }
+  }
+};
+
+function onShutdown(context) {
+  // event on the plugin that's being updated
+}
+
+function onStartup(context) {
+  // event on the plugin that is installed
+}
+
+/***/ })
+/******/ ]);
+  if (key === 'default' && typeof exports === 'function') {
+    exports(context);
+  } else {
+    exports[key](context);
+  }
+}
+that['onRun'] = __skpm_run.bind(this, 'default')
