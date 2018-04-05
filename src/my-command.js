709af86a87f458ce7d7c2108c0e92d51d2d62804
Roman Shamin <al4emist.artway@gmail.com>
2018-04-05 09:31:14 +0300

    Version 1.0

--- src/my-command.js
+++ src/my-command.js
@@ -1,3 +1,35 @@
 export default function(context) {
-  context.document.showMessage("It's alive 🙌")
+  const selectedLayers = context.selection
+
+  if (selectedLayers.length === 0) {
+    context.document.showMessage('Select at least one bitmap')
+  } else {
+    const predicateBitmapLayers = NSPredicate.predicateWithFormat("self isMemberOfClass:%@", MSBitmapLayer.class())
+    const bitmapLayers = selectedLayers.filteredArrayUsingPredicate(predicateBitmapLayers)
+
+    const bitmapLayersLength = bitmapLayers.length
+
+    if (bitmapLayersLength === 0) {
+      context.document.showMessage('Select at least one bitmap')
+    } else {
+
+      bitmapLayers.forEach(
+        layer => layer.correctInvalidGamma()
+      )
+
+      if (bitmapLayersLength === 1) {
+        context.document.showMessage(`The bitmap has been corrected`)
+      } else {
+        context.document.showMessage(`${bitmapLayersLength} bitmaps have been corrected`)
+      }
+    }
+  }
+}
+
+function onShutdown(context) {
+  // event on the plugin that's being updated
+}
+
+function onStartup(context) {
+  // event on the plugin that is installed
 }
